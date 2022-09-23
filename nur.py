7b597615a8f718d9f90b41634279468388ba8a30
aisyahtrisakti <114251251+aisyahtrisakti@users.noreply.github.com>
2022-09-23 10:27:30 -0700

    Add files via upload

---
+++ nur.py
@@ -0,0 +1,9 @@
+import time
+
+x=input("masukkan string pertama :")
+y=input("masukkan string kedua   :")
+z=input("masukkan string ketiga  :")
+
+print("hasil penggabungan tiga string tersebut adalah "," ".join([x,y,z]))
+
+time.sleep(50)
